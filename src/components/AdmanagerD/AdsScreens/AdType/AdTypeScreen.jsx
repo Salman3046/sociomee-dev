923a1413d627eddd1e984281974885fdbbc41370
Salman <charliekhan7410@gmail.com>
2022-10-04 19:01:07 +0530

    Single Image Ad

--- src/components/AdmanagerD/AdsScreens/AdType/AdTypeScreen.jsx
+++ src/components/AdmanagerD/AdsScreens/AdType/AdTypeScreen.jsx
@@ -1,27 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { getSingleAdTypeOfType, loadAdType } from '../../../../Services/Actions/AdManager/getAdTypeAction';
 import AdmanagerHeaderR from '../../AdmanagerHeaderR/AdmanagerHeaderR'
 
 const AdTypeScreen = () => {
+    const { adType } = useSelector((state) => state.getTypeData);
     const { singleTypeOfType } = useSelector(state => state.getTypeData)
+    const [singleTypeId,setSingleTypeId]=useState({id:'',format:''})
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const { id } = useParams();
 
+    const adTypeData=useMemo(()=>{
+        return adType.find(ad=>ad.id===id)
+    },[adType])
+    
     // 
-    const [state, setState] = useState({
+    const [state, setState] = useState({});
 
-    });
+    const submitHandler =(e)=>{
+        e.preventDefault();
+        if (!singleTypeId.id) {
+          alert("Please Select Sub Type");
+        } else {
+          navigate(`/Ad/Display/${singleTypeId.format}`,{state:{typeId:adTypeData.id,subTypeId:singleTypeId.id}});
+        }
+    }
 
-    // TAD YPE OF TYPES
-    useEffect(() => {
-        dispatch(getSingleAdTypeOfType(id))
-    }, [])
     useEffect(() => {
         setState({ ...singleTypeOfType })
     }, [singleTypeOfType])
-
+    
+    // TAD YPE OF TYPES
+    useLayoutEffect(() => {
+        dispatch(getSingleAdTypeOfType(id))
+        dispatch(loadAdType());
+    }, [id])
+    
     return (
         <>
             <AdmanagerHeaderR />
@@ -38,22 +54,22 @@ const AdTypeScreen = () => {
                                 <i className="fa fa-ellipsis-h"></i>
                             </div>
                         </div>
-                        <div className="create-add-main">
-                            <div className="create-add">
-                                <div className="create-add-one">
-                                    <img src="/assets/images/adIcon/folder.png" alt="" />
-                                    <p className='ml-2'>Create Ad</p>
-                                </div>
-                                <div className="create-add-second">
-                                    <i className="fa fa-ellipsis-h"></i>
-                                </div>
+
+                        <div className="desh-icon-main pl-2">
+                            <div className="desh-icon">
+                                <img src="/assets/images/adIcon/folder.png" alt="" />
+                                <p className='ml-2'>Create Ad</p>
+                            </div>
+                            <div className="desh-second">
+                                <i className="fa fa-ellipsis-h"></i>
                             </div>
                         </div>
+
                         <div className="create-add-main">
                             <div className="create-add">
                                 <div className="create-add-one">
                                     <img src="/assets/images/adIcon/folder.png" alt="" />
-                                    <p className='ml-2'>Create Ad</p>
+                                    <p className='ml-2'>{adTypeData?.adTypes} - ({adTypeData?.adMastrerType.name})</p>
                                 </div>
                                 <div className="create-add-second">
                                     <i className="fa fa-ellipsis-h"></i>
@@ -80,22 +96,23 @@ const AdTypeScreen = () => {
                             <div className="row ad-types-of-type-map pad-zero">
                                 
                                 {
-                                    singleTypeOfType.successResult?.map((type) => {
+                                    singleTypeOfType.successResult?.map(({id,name,file,note,adFormat}) => {
                                         return (
-                                            <div className="col-lg-6 pad-zero">
-                                                <Link to={`/Ad/Display/${type.adFormat}`} key={type.id}>
-                                                    <div className="ad-type d-flex ad-types-type">
+                                            <div className={'col-lg-6 pad-zero'} key={id}>
+                                                <div>
+                                                    <div className={`ad-type d-flex ad-types-type border ${singleTypeId.id===id && 'border-success'}`} onClick={()=>setSingleTypeId({id:id,format:adFormat})} role='button'>
                                                         <div className='mr-3'>
-                                                            <img src="/assets/images/adIcon/Frame 1105.png"
-                                                            alt="ad image"
+                                                            <img src={file}
+                                                            alt="ad"
+                                                            className='ch-150'
                                                              />
                                                         </div>
                                                         <div className='ad-types-type-dec'>
-                                                            <p className='type mt-2'>{type.name}</p>
-                                                            <p className='dec-types mt-2 pad-zero'>{type.note}</p>
+                                                            <p className='type mt-2'>{name}</p>
+                                                            <p className='dec-types mt-2 pad-zero'>{note}</p>
                                                         </div>
                                                     </div>
-                                                </Link>
+                                                </div>
                                             </div>
                                         )
                                     })
@@ -108,9 +125,9 @@ const AdTypeScreen = () => {
                                 <Link to="/Ad/LeadGeneration" className="btn-cancel">
                                     Cancel
                                 </Link>
-                                <Link to="/Ad/SingleImage" className='btn-next'>
+                                <button className='btn-next without-input-fill ml-2' disabled={!singleTypeId.id?true:false} onClick={submitHandler}>
                                     Next
-                                </Link>
+                                </button>
                             </div>
                         </div>
                     </div>
